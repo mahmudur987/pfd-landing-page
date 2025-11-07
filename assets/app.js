416e1babd5bb513bd850bcf2b1300e996a635207
mahmudur987 <safemahmud987@gmail.com>
2025-11-07 17:36:19 +0600

    update address

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,29 @@
-!
+const sendButton = document.getElementById("sendMail");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+
+sendButton.addEventListener("click", () => {
+  if (
+    name.value == "" ||
+    email.value == "" ||
+    phone.value == "" ||
+    message.value == ""
+  ) {
+    alert("Please fill all the fields");
+  } else {
+    alert("Your message has been sent successfully");
+    const data = {
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      message: message.value,
+    };
+    console.log(data);
+    name.value = "";
+    email.value = "";
+    phone.value = "";
+    message.value = "";
+  }
+});
